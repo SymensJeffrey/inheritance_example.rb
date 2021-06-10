0492761d498a61ffd29fcf08bfbf972e9cc5669e
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-09 19:09:22 -0500

    dry up code

--- bicycle.rb
+++ bicycle.rb
@@ -21,26 +21,14 @@ class Car
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Car
   def ring_bell
     puts "Ring ring!"
   end
 end
 
+bike1 = Bike.new
+p bike1.accelerate
+
+car1 =Car.new
+p car1.accelerate
