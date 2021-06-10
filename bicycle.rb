be3e5894c35717eed450e37b3b80d647cb4970ab
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-09 19:17:36 -0500

    end of exeercise

--- bicycle.rb
+++ bicycle.rb
@@ -1,4 +1,4 @@
-class Car
+class Vehicle
   def initialize
     @speed = 0
     @direction = 'north'
@@ -16,24 +16,23 @@ class Car
     @direction = new_direction
   end
 
-  def honk_horn
-    puts "Beeeeeeep!"
-  end
 end
 
-class Bike < Car
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike1 = Bike.new
-# p bike1.accelerate
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
 
+bike1 = Bike.new
 car1 =Car.new
-# p car1.accelerate
 
 p car1.honk_horn
-# p car1.ring_bell
 
 p bike1.ring_bell
